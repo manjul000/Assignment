0e12af15762df94113d92cb3130a10bdae60d347
manjul000 <manjuldahal1@gmail.com>
2022-09-05 15:59:35 +0545

    :hammer: Finished.

---
+++ odd_filter.py
@@ -0,0 +1,18 @@
+# Filter even number from a list
+import random
+
+num_list = []
+
+for i in range (0, 100, 1):
+    random_num = random.randint(0, 100)
+    num_list.append(random_num)
+
+filtered_list = []
+length = len(num_list)
+
+for num in range (0, length, 1):
+    val = int(num_list[num])
+    if val % 2 == 1:
+        filtered_list.append(val)
+
+print(filtered_list)
