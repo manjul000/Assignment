49df4cdfdf718b35df82024eaaf32a99b3c4b140
manjul000 <manjuldahal1@gmail.com>
2022-09-05 15:34:21 +0545

    :hammer: Finished.

--- num_guess.py
+++ num_guess.py
@@ -1,1 +1,18 @@
 # guessing game
+import random
+
+lower_num = int(input("PLeas enter a lower limit : "))
+upper_num = int(input("Please enter an upper limit : "))
+
+random_num = random.randint(lower_num, upper_num)
+
+guess_num = int(input(f"Please enter your guess number ({lower_num}-{upper_num}): "))
+
+while True:
+    if guess_num > random_num:
+        guess_num = int(input("Try lower\n--> "))
+    elif guess_num < random_num:
+        guess_num = int(input("Try higher\n--> "))
+    elif guess_num == random_num:
+        print("You guessed correctly.")
+        break
